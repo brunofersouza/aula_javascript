9268c4680c6eb1ae6e57947e9993aad6bc691533
BRUNO <brunofersouza@hotmail.com>
2020-04-05 22:46:55 -0300

    comandos basicas de JAVASCRIPT

---
+++ javascript/main.js
@@ -0,0 +1,148 @@
+function clicou (){
+    document.getElementById("agradecimento").innerHTML = "<b>OBRIGADO POR CLICAR</br>";
+ //   console.log(document.getElementById("agradecimento"));
+
+
+}
+/*
+function redirecionar(){
+    window.open("http://devfuria.com.br/git/tutorial-iniciando-git/");
+ //   window.location.href = "https://www.globo.com/";
+
+}
+*/
+function funcaochange(elemento){
+ console.log(elemento.valeu)
+
+
+}
+
+
+
+
+function trocar (elemento){
+  //  document.getElementById("mousemover").innerHTML = "OBRIGADO PRO PASSAR O MOUSE AQUI !!!";
+   // alert("trocar TEXTO");
+   elemento.innerHTML = "OBRIGADO PRO PASSAR O MOUSE AQU";
+
+}
+
+function voltar(elemento){
+ //   document.getElementById("mousemover").innerHTML = " PASSA O MOUSE AQUI !!!";
+ elemento.innerHTML = "  PASSA O MOUSE AQUI !!";
+}
+
+function load(){
+    alert("Pagina carregada");
+}
+
+/*
+function clicar(){
+    alert("Obrigado por clicar");
+}
+
+
+function soma (n1,n2){
+    return n1+n2 ;   
+}
+
+function validaIdade (idade){
+    var validar;
+    if (idade >= 18){
+        validar= true
+    }else{
+        validar =false
+    }
+    return validar;
+}
+var idade = prompt  ("QUAL E SUA IDADE");
+console.log(validaIdade(idade));
+
+
+
+function setReplace(frase,nome,novo_nome){
+    return frase.setReplace(nome, novo_nome)
+
+}
+
+alert (soma(5,10));
+
+alert ("VAI JAPAO ","JAPAO","BRASIL");
+
+
+
+
+
+var d =new Date();
+alert(d.getDay);
+
+
+
+
+var count;
+for (count=0;count<=5; count++){
+    alert(count);
+}
+
+var count = 0;
+while (count <= 5){
+     console.log(count);
+      count ++;
+      alert(count);
+}; 
+
+
+
+
+var idade = prompt("QUAL E A SUA IDADE ");
+
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade ");
+}else{
+    alert("menor de idade");
+};
+
+
+
+
+
+var frutas = [(nome ="maca",cor ="vermelha"),(nome ="uva",cor ="roxa")];
+console.log (frutas);
+alert (frutas[1].nome);
+
+
+
+
+var nome = "BRUNO FERNANDES";
+var idade = 26;
+var  frase = "JAPÃO E O MELHOR TIME DO MUNDO"
+
+var n1 = 4;
+var n2 = 6;
+
+
+//alert (nome + " TEM  " + idade + "  ANOS" );
+console.log (nome);
+console.log (idade);
+console.log (frase.replace("JAPÃO","TIMAO")); 
+
+*/
+
+//alert (n1+n2);
+//    var lista = ["maça","péra","uva."];
+//console.log(lista[1]);
+//lista.push("limao");
+//alert (lista[1]);
+
+//console.log(lista);
+
+//console.log(lista.length)
+
+//console.log(lista.reverse());
+
+//console.log(lista.toString());
+
+//console.log(lista.join(" . "))
+
+
